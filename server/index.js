087b87c80d0b9bc35ef8a818b02cac68885aabe7
d1nsol <jinsik0126@gmail.com>
2020-10-25 15:49:12 +0900

    ...

--- server/index.js
+++ server/index.js
@@ -41,17 +41,17 @@ app.use('/api/users', require('./routes/users'));
 app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
 
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+//   // Set static folder   
+//   // All the javascript and css files will be read and served from this folder
+//   app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+//   // index.html for all page routes    html or routing and naviagtion
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 5000
 
